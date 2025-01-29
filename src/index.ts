d567dd7780c261f27e72906b40a8fb799f6b7e90
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2025-01-29 17:02:10 -0600

    express ready

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    app.listen(port, () => {
+      console.log(`Server running in port http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+start();
